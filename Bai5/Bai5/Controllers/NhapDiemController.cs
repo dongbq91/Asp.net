cd583e03a631b3741ecf3858ecfe7378dea679ff
bachquocdong <57605481+dongbq91@users.noreply.github.com>
2022-01-11 09:45:45 +0700

    Add bai 5

---
+++ Bai5/Bai5/Controllers/NhapDiemController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bai5.Models;
+
+namespace Bai5.Controllers
+{
+    public class NhapDiemController : Controller
+    {
+        // GET: NhapDiem
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Save(SinhVien sv)
+        {
+            String path = Server.MapPath("~/student.txt");
+            string[] lines = { sv.Id, sv.Name, sv.Diem.ToString() };
+            System.IO.File.WriteAllLines(path, lines);
+            ViewBag.HanhDong = "Da gi file!";
+            return View("Index");
+        }
+        public ActionResult Open(SinhVien sv)
+        {
+            String path = Server.MapPath("~/student.txt");
+            string[] lines = System.IO.File.ReadAllLines(path);
+            sv.Id = lines[0];
+            sv.Name = lines[1];
+            sv.Diem = Convert.ToDouble(lines[2]);
+            ViewBag.ThongTin = "Ma sinh vien :" + sv.Id + "- Ho ten : " + sv.Name + "- Diem :" + sv.Diem;
+            ViewBag.HanhDong = "Da doc tu file!";
+            return View("Index");
+        }
+    }
+}
---
+++ Bai5/Bai5/Controllers/StaffController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bai5.Models;
+
+namespace Bai5.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Save()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Save(FormCollection f)
+        {
+            var fhinh = Request.Files["myfileImage"];
+
+            var pathhinh = Server.MapPath("~/Images" + fhinh.FileName);
+            fhinh.SaveAs(pathhinh);
+
+            string path = Server.MapPath("~/myfileImage.txt");
+            string[] info = { f["txtID"], f["txtName"], f["txtDate"], f["txtSalary"], fhinh.FileName };
+            System.IO.File.WriteAllLines(path, info);
+            return View("Index");
+
+        }
+        public ActionResult open()
+        {
+            string path = Server.MapPath("~/Staffinfo.txt");
+            string[] info = System.IO.File.ReadAllLines(path);
+            Staff s = new Staff();
+            s.StaffID = int.Parse(info[0]);
+            s.StaffName = info[1];
+            s.BirthofDate = DateTime.Parse(info[2]);
+            s.Salary = decimal.Parse(info[3]);
+            s.StaffImage = info[4];
+
+            ViewBag.id = s.StaffID;
+            ViewBag.name = s.StaffName;
+            ViewBag.birthday = s.BirthofDate.ToString("yyyy-MM-dd");
+            ViewBag.salary = s.Salary;
+            ViewBag.image = "../../Images/" + s.StaffImage;
+
+            return View("Index");
+        }
+    }
+}
